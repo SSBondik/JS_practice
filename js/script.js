af958e2b4a73ce4bcfb006d2282137b1ff4e0a2e
Alex Bondarenko <alexbondarenko0212@gmail.com>
2022-08-16 20:41:26 +0300

    New JS project

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// alert("Hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "Да");
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt("Ваше имя?", "");
+answers[1] = prompt("Ваша фамилия?", "");
+answers[2] = prompt("Сколько вам лет?", "");
+
+console.log(answers);
+//['Алекс', 'Бонд', '21']
