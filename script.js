60f69a41402c74c37fcedac76a92e0266de0cb8a
Alex Bondarenko <alexbondarenko0212@gmail.com>
2022-08-18 00:46:19 +0300

    task with cycles

--- script.js
+++ script.js
@@ -23,22 +23,51 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = +prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = +prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = +prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         continue;
+//     }
+
+//     console.log(i);
+// } 
+
+
+let result = '';
+const lgth = 7;
+
+for (let i = 1; i < lgth; i++) {
+
+    for(let j = 0; j < i; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
+
+console.log(result);
+// *
+// **
+// ***
+// ****
+// *****
+// ******
