478de05465a447e549f39d1dabb514d120ab999c
Alex Bondarenko <alexbondarenko0212@gmail.com>
2022-09-18 12:19:53 +0300

    calculate volume and area

--- script.js
+++ script.js
@@ -257,71 +257,84 @@
 // console.log(getMathResult(14,8));
 
 
-let numberOfFilms;
+// let numberOfFilms;
 
-function start () {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// function start () {
+//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-start();
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+//     }
+// }
+// start();
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
 
-function rememberMyFilms() {
-    for(let  i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
+// function rememberMyFilms() {
+//     for(let  i = 0; i < 2; i++) {
+//         const a = prompt('Один из последних просмотренных фильмов?', ''),
+//               b = prompt('На сколько оцените его?', '');
                   
-        if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
-            personalMovieDB.movies[a] = b;
-            console.log('done!');
-        } else {
-            console.log('error');
-            i--;
-        }      
-    } 
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('done!');
+//         } else {
+//             console.log('error');
+//             i--;
+//         }      
+//     } 
         
-}
-// rememberMyFilms();
+// }
+// // rememberMyFilms();
 
 
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        alert('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        alert('Вы классический зритель');
-    } else if (personalMovieDB.count >= 30) {
-        alert('Вы киноман');
-    } else {
-        alert("Произошла ошибка");
-    }
+// function detectPersonalLevel() {
+//     if (personalMovieDB.count < 10) {
+//         alert('Просмотрено довольно мало фильмов');
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//         alert('Вы классический зритель');
+//     } else if (personalMovieDB.count >= 30) {
+//         alert('Вы киноман');
+//     } else {
+//         alert("Произошла ошибка");
+//     }
     
+// }
+
+// // detectPersonalLevel();
+
+// function showMyDB(hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDB);
+//     }
+// }
+
+// showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres() {
+//     for(let i = 1; i < 4; i++) {
+//         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номер ${i}`);
+//     }
+// }
+
+// writeYourGenres();
+
+
+
+function calculateVolumeAndArea(length) {
+ if(typeof(length) !== 'number' || length < 0 || !Number.isInteger(length)) {
+    return "При вычислении произошла ошибка";
+ }
+ let V = length * length * length;
+ let S = 6 * (length * length);
+ return `Объем куба: ${V}, площадь всей поверхности: ${S}`;
+
 }
-
-// detectPersonalLevel();
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres() {
-    for(let i = 1; i < 4; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номер ${i}`);
-    }
-}
-
-writeYourGenres();
+calculateVolumeAndArea(81);
