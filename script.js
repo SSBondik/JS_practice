5792552ddddfef6b6c3fcaf1af4f17798f709324
Alex Bondarenko <alexbondarenko0212@gmail.com>
2022-09-24 03:08:50 +0300

    Practice, ch4. Using Objects

--- script.js
+++ script.js
@@ -516,40 +516,141 @@
 // }
 // standardizeStrings(favoriteCities);
 
-const someString = 'This is some strange string';
+// const someString = 'This is some strange string';
 
-function reverse(str) {
-    if(typeof(str) !== 'string') {
-        return "Ошибка!";
-    }
-    return str.split('').reverse().join('');
+// function reverse(str) {
+//     if(typeof(str) !== 'string') {
+//         return "Ошибка!";
+//     }
+//     return str.split('').reverse().join('');
 
-    // let arr = [];
-    // for(let i = 0; i < str.length; i++) {
-    //     arr[i] = str[i];  
-    // }
-    // let arrReverse = arr.reverse();
-    // let result = arrReverse.join('');
-    // return result;
-}
-console.log(reverse(someString));
+//     // let arr = [];
+//     // for(let i = 0; i < str.length; i++) {
+//     //     arr[i] = str[i];  
+//     // }
+//     // let arrReverse = arr.reverse();
+//     // let result = arrReverse.join('');
+//     // return result;
+// }
+// console.log(reverse(someString));
 
 
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+// const baseCurrencies = ['USD', 'EUR'];
+// const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 
-function availableCurr(arr, missingCurr) {
-    let str = 'Доступные валюты:\n';
-    if(arr.length === 0) {
-        return 'Нет доступных валют';
-    }
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] === missingCurr) {
-            continue;
+// function availableCurr(arr, missingCurr) {
+//     let str = 'Доступные валюты:\n';
+//     if(arr.length === 0) {
+//         return 'Нет доступных валют';
+//     }
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] === missingCurr) {
+//             continue;
+//         } else {
+//             str += `${arr[i]}\n`;
+//         }
+//     }
+//     return str;
+// }
+// console.log(availableCurr(['UAH', 'RUB', 'CNY'], 'RUB'));
+
+
+/* Задание на урок:
+
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+// Код возьмите из предыдущего домашнего задания
+
+
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function() {
+        for(let  i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+                  b = prompt('На сколько оцените его?', '');
+                      
+            if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+                personalMovieDB.movies[a] = b;
+                console.log('done!');
+            } else {
+                console.log('error');
+                i--;
+            }      
+        }
+    },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            alert('Просмотрено довольно мало фильмов');
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+            alert('Вы классический зритель');
+        } else if (personalMovieDB.count >= 30) {
+            alert('Вы киноман');
         } else {
-            str += `${arr[i]}\n`;
+            alert("Произошла ошибка");
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function() {
+        // for(let i = 0; i < 3; i++) {
+        //     let genre = prompt(`Ваш любимый жанр под номер ${i + 1}`);
+        //     personalMovieDB.genres[i] = genre;
+        //     if(genre === '' || genre == null) {
+        //         console.log('Вы ввели некоректные данные!');
+        //         i--;
+        //     } else {
+        //         personalMovieDB.genres[i] = genre;
+        //     }
+        // }
+            for(let i = 1; i < 2; i++) {
+                let genre = prompt(`Напишите ваши любимые жанры через запятую`).toLowerCase();
+
+                if(genre === '' || genre == null) {
+                    console.log('Вы ввели некоректные данные!');
+                    i--;
+                } else {
+                    personalMovieDB.genres = genre.split(', ');
+                    personalMovieDB.genres.sort();
+                }
+            }
+
+        return personalMovieDB.genres.forEach((value, index) => {
+            console.log(`Любимый жанр #${index + 1} - это ${value}`);
+        });
+
+        
+    },
+    toggleVisibleMyDB: function() {
+        if(personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
         }
     }
-    return str;
-}
-console.log(availableCurr(['UAH', 'RUB', 'CNY'], 'RUB'));
+};
+personalMovieDB.writeYourGenres();
+console.log(personalMovieDB.genres);
